41247f73d766c547d2c277126c1b1106ad17df59
James Chen <james@innovue.ltd>
2022-07-20 10:42:27 +0800

    (^) updated uglify config slightly

--- bundle.mjs
+++ bundle.mjs
@@ -1,17 +1,17 @@
-import fs from 'fs/promises'
-import theredoc from 'theredoc'
-import mkdirp from 'mkdirp'
-import { execSync } from 'child_process'
-import { build } from 'vite'
-import { program } from 'commander'
+import fs from "fs/promises"
+import theredoc from "theredoc"
+import mkdirp from "mkdirp"
+import { execSync } from "child_process"
+import { build } from "vite"
+import { program } from "commander"
 
 program
-  .option('-m, --moduleName <name>', 'module to bundle')
-  .option('-d, --asDefault', 're-export module using default export')
-  .option('-p, --peers', 'pack peer dependencies into same bundle')
+  .option("-m, --moduleName <name>", "module to bundle")
+  .option("-d, --asDefault", "re-export module using default export")
+  .option("-p, --peers", "pack peer dependencies into same bundle")
   .option(
-    '-D, --manualDeps <deps>',
-    'manually specify dependencies, separated by commas'
+    "-D, --manualDeps <deps>",
+    "manually specify dependencies, separated by commas"
   )
 
 program.parse()
@@ -25,8 +25,8 @@ if (!moduleName) {
 const config = {
   build: {
     lib: {
-      entry: 'src/app.ts',
-      name: 'vendor',
+      entry: "src/app.ts",
+      name: "vendor",
       fileName: (format) => `${outputfileName}.${format}.js`,
     },
   },
@@ -40,7 +40,7 @@ execSync(`yarn add ${moduleName}`)
 
 // process peer dependencies
 const infoCmd = `yarn info ${moduleName} peerDependencies`
-const pkgInfo = execSync(infoCmd, { encoding: 'utf8' })
+const pkgInfo = execSync(infoCmd, { encoding: "utf8" })
 const regex = /\{[\n\r\d\s\w\W]*\}/
 let peerDeps = []
 if (regex.test(pkgInfo)) {
@@ -54,13 +54,13 @@ if (regex.test(pkgInfo)) {
     .map((s) => /\s+([\w\W]+):/.exec(s)[1])
 
   // install all peer deps as dev deps
-  execSync(`yarn add -D ${peerDeps.join(' ')}`)
+  execSync(`yarn add -D ${peerDeps.join(" ")}`)
   config.rollupOptions.external = [config.rollupOptions.external, ...peerDeps]
 }
 
 if (manualDeps) {
-  const deps = manualDeps.split(',')
-  execSync(`yarn add -D ${deps.join(' ')}`)
+  const deps = manualDeps.split(",")
+  execSync(`yarn add -D ${deps.join(" ")}`)
   config.rollupOptions.external = [config.rollupOptions.external, ...deps]
 }
 
@@ -74,19 +74,19 @@ const entry = asDefault
   : theredoc`
       export * from '${moduleName}'
     `
-await mkdirp('src')
-await fs.writeFile('src/app.ts', entry, 'utf8')
-const outputfileName = moduleName.replace('/', '-').replace('@', '')
+await mkdirp("src")
+await fs.writeFile("src/app.ts", entry, "utf8")
+const outputfileName = moduleName.replace("/", "-").replace("@", "")
 
 // build bundle
 
 await build(config)
-await fs.rm('src', { recursive: true })
+await fs.rm("src", { recursive: true })
 execSync(
-  `yarn remove ${moduleName} ${peerDeps.join(' ')} ${
-    manualDeps?.split(',').join(' ') ?? ''
+  `yarn remove ${moduleName} ${peerDeps.join(" ")} ${
+    manualDeps?.split(",").join(" ") ?? ""
   }`
 )
 execSync(
-  `yarn uglifyjs dist/${outputfileName}.es.js --compress --mangle -o dist/${outputfileName}.es.min.js`
+  `yarn uglifyjs dist/${outputfileName}.es.js --compress --mangle --module -o dist/${outputfileName}.es.min.js`
 )
